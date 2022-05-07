8f533c1ff04329eddc54c735c2ea1b76399bf859
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2022-05-07 17:09:11 -0300

    Cada año tendra su curso correspondiente

--- app/src/main/java/com/gomara/UI/activity_login.java
+++ app/src/main/java/com/gomara/UI/activity_login.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -14,6 +15,7 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.gomara.R;
+import com.gomara.Prosecer.Horarios;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -27,8 +29,7 @@ public class activity_login extends AppCompatActivity {
     private Spinner spAnio , spCurso;
     private Button    btLogin;
 
-
-    private String stringAnio = "",stringCurso = "";
+    private String stringAnio,stringCurso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,50 +42,62 @@ public class activity_login extends AppCompatActivity {
         iconoGomara = findViewById(R.id.iconGomaraLogin);
         btLogin = findViewById(R.id.btLogin);
 
-        btLogin.setText("Entrar");
-
         //Gomara school Icon
         iconoGomara.setImageResource(R.drawable.gomara);
+
+        //Agregamos la lista de años
+        String[] anio  = getResources().getStringArray(R.array.ANIO);
+        ArrayAdapter<CharSequence> adapterAnio = new ArrayAdapter<>(activity_login.this, R.layout.dropdownitem, anio);
+        spAnio.setAdapter(adapterAnio);
+
+        spAnio.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String[] curso;
+                switch (parent.getItemAtPosition(position).toString()){
+                    case "1":
+                    case "2":
+                    case "3":
+                        curso = new String[]{"a","b","c","d"};
+                        break;
+
+                    case "4":
+                    case "5":
+                        curso = new String[]{"a", "b", "c"};
+                        break;
+
+                    case "6":
+                        curso = new String[]{"a"};
+                        break;
+                    default:
+                        throw new IllegalStateException("Unexpected value: " + parent.getItemAtPosition(position).toString());
+                }
+                ArrayAdapter<CharSequence> adapterCurso = new ArrayAdapter<>(activity_login.this, R.layout.dropdownitem, curso);
+                spCurso.setAdapter(adapterCurso);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         btLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 //Get of the fields
                 String Anio = spAnio.getSelectedItem().toString();
                 String Curso = spCurso.getSelectedItem().toString();
 
-                //if we get Username and password different of the null
-                if (!Anio.equals("") && !Curso.equals("")) {
-
-                    if (new view_horarios.Horarios().isCorrect(Anio, Curso)) {
-                        //Guardar en su dispositivo sus Datos para luego entrar directamente al viewHorarios
-                        guardarFichero(Anio,Curso);
-                        Intent i = new Intent(activity_login.this , view_horarios.class);
-                        i.putExtra("intentAnio",Anio);
-                        i.putExtra("intentCurso",Curso);
-                        startActivity(i);
-                    } else
-                        Toast.makeText(activity_login.this, "NO EXISTE", Toast.LENGTH_SHORT).show();
-
-                } else
-                    Toast.makeText(activity_login.this, "Campos Vacios", Toast.LENGTH_SHORT).show();
+                //Guardar en su dispositivo sus Datos para luego entrar directamente al viewHorarios
+                guardarFichero(Anio,Curso);
+                Intent i = new Intent(activity_login.this , view_horarios.class);
+                i.putExtra("intentAnio",Anio);
+                i.putExtra("intentCurso",Curso);
+                startActivity(i);
             }
         });
 
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        String[] anio = getResources().getStringArray(R.array.ANIO);
-        String[] curso = getResources().getStringArray(R.array.CURSO);
-
-        ArrayAdapter<CharSequence> adapterAnio = new ArrayAdapter<>(activity_login.this, R.layout.dropdownitem, anio);
-        ArrayAdapter<CharSequence> adapterCurso = new ArrayAdapter<>(activity_login.this, R.layout.dropdownitem, curso);
-
-        spAnio.setAdapter(adapterAnio);
-        spCurso.setAdapter(adapterCurso);
     }
 
     public void guardarFichero(String anio, String curso){
